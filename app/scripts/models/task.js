dcd0a9c04e4490122cc0eb3ee51e6f67718af383
Cedric Yao <raspar@gmail.com>
2013-12-10 12:11:15 -0600

    added default to fixtures for task

--- app/scripts/models/task.js
+++ app/scripts/models/task.js
@@ -9,4 +9,23 @@ Yotest.Task = DS.Model.extend({
     client: DS.attr()
 });
 
-
+Yotest.Task.FIXTURES = [
+    {
+        id: 1,
+        sow: 'UDELL004',
+        name: 'Dec 2013 - ALERT: Large All Store Spend Processed Delta',
+        client: 'Udell'
+    },
+    {
+        id: 2,
+        sow: 'Headspring Dallas',
+        name: 'Planning',
+        client: '- Company'
+    },
+    {
+        id: 3,
+        sow: 'UDELL004',
+        name: 'Dec 2013 - BenefitsIssued Report ',
+        client: 'Udell'
+    }
+];
